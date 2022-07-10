d57389062ece0ebd5c805e38ee3968320389600b
o0hana <2487311872@qq.com>
2022-07-10 04:44:56 +0000

    Update at 2022-07-10T04:44:56.740246Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -22,7 +22,7 @@ public class MaxValues {
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = 1.7976931348623157e308f;
+    public static double maxDouble = 1.7976931348623157e308;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
