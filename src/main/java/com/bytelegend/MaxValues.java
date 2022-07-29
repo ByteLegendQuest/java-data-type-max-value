bc3242480f754f17bac44b6400752c7385f3b0c5
SaraadKun <35584589+SaraadKun@users.noreply.github.com>
2022-07-29 12:04:10 +0000

    Update at 2022-07-29T12:04:10.882685Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -22,7 +22,7 @@ public class MaxValues {
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = DOUBLE.MAX_VALUE;
+    public static double maxDouble = Double.MAX_VALUE;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
