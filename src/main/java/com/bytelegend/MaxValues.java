fb12b02a31287efc1fdb4a3623e971cc07f60e69
agirnob <kakcayigitbaran@gmail.com>
2022-08-15 06:23:22 +0000

    Update at 2022-08-15T06:23:22.029445Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -1,32 +1,33 @@
 package com.bytelegend;
 public class MaxValues {
-    // byte类型能存储的最大值是？
-    // The maximum value a byte type can store
-    public static byte maxByte = ?;
+        // byte类型能存储的最大值是？
+        // The maximum value a byte type can store
+        public static byte maxByte = Byte.MAX_VALUE;
 
-    // short类型能存储的最大值是？
-    // The maximum value a short type can store
-    public static short maxShort = ?;
+        // short类型能存储的最大值是？
+        // The maximum value a short type can store
+        public static short maxShort = Short.MAX_VALUE;
 
-    // int类型能存储的最大值是？
-    // The maximum value an int type can store
-    public static int maxInt = ?;
+        // int类型能存储的最大值是？
+        // The maximum value an int type can store
+        public static int maxInt = Integer.MAX_VALUE;
 
-    // long类型能存储的最大值是？
-    // The maximum value an long type can store
-    public static long maxLong = ?;
+        // long类型能存储的最大值是？
+        // The maximum value an long type can store
+        public static long maxLong = Long.MAX_VALUE;
 
-    // float类型能存储的最大值是？
-    // The maximum value an float type can store
-    public static float maxFloat = ?;
+        // float类型能存储的最大值是？
+        // The maximum value an float type can store
+        public static float maxFloat = Float.MAX_VALUE;
 
-    // double类型能存储的最大值是？
-    // The maximum value an double type can store
-    public static double maxDouble = ?;
+        // double类型能存储的最大值是？
+        // The maximum value an double type can store
+        public static double maxDouble = Double.MAX_VALUE;
 
-    // char类型能存储的最大值是？
-    // The maximum value an char type can store
-    public static char maxChar = ?;
+        // char类型能存储的最大值是？
+        // The maximum value an char type can store
+        public static char maxChar = Character.MAX_VALUE;
+
 
     public static void main(String[] args) {
         System.out.println("maxByte=" + maxByte);
