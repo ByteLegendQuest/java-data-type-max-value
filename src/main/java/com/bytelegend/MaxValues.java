0d54e9db586e509c66e6e5e6c9aa01e88867be2a
slsy0 <1485807135@qq.com>
2022-08-31 09:11:44 +0000

    Update at 2022-08-31T09:11:44.150305Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -18,11 +18,11 @@ public class MaxValues {
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat = 3.4028235E38;
+    public static float maxFloat = 3.4028235E38f;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = 1.7976931348623157E308;
+    public static double maxDouble = 1.7976931348623157E308D;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
