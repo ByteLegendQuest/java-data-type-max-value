74b14dbc06e9d72b21cb94320f14d294bd545621
Mooos-MoSheng <1297393910@qq.com>
2022-10-10 09:52:29 +0000

    Update at 2022-10-10T09:52:29.898001Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -1,31 +1,31 @@
 public class Main {
     // byte类型能存储的最大值是？
     // The maximum value a byte type can store
-    public static byte maxByte = 127;
+    public static byte maxByte = Byte.MAX_VALUE;
 
     // short类型能存储的最大值是？
     // The maximum value a short type can store
-    public static short maxShort = 32767;
+    public static short maxShort = Short.MAX_VALUE;
 
     // int类型能存储的最大值是？
     // The maximum value an int type can store
-    public static int maxInt = 2147483647;
+    public static int maxInt = Integer.MAX_VALUE;
 
     // long类型能存储的最大值是？
     // The maximum value an long type can store
-    public static long maxLong = 9223372036854775807l;
+    public static long maxLong = Long.MAX_VALUE;
 
     // float类型能存储的最大值是？
     // The maximum value an float type can store
-    public static float maxFloat = (float) 3.4E+38;
+    public static float maxFloat = Float.MAX_VALUE;
 
     // double类型能存储的最大值是？
     // The maximum value an double type can store
-    public static double maxDouble = 1.7E+308;
+    public static double maxDouble = Double.MAX_VALUE;
 
     // char类型能存储的最大值是？
     // The maximum value an char type can store
-    public static char maxChar = 65535;
+    public static char maxChar = Character.MAX_VALUE;
 
     public static void main(String[] args) {
         System.out.println("maxByte=" + maxByte);
