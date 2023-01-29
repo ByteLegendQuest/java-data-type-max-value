98525e656c1b246073dddb0e86d123107003859b
andyliukun <744059787@qq.com>
2023-01-29 00:26:30 +0000

    Update at 2023-01-29T00:26:30.939501Z

--- src/main/java/com/bytelegend/MaxValues.java
+++ src/main/java/com/bytelegend/MaxValues.java
@@ -6,7 +6,7 @@ public class MaxValues {
 
     // short类型能存储的最大值是？
     // The maximum value a short type can store
-    public static short maxShort = Short.MAX_VALUE?;
+    public static short maxShort = Short.MAX_VALUE;
 
     // int类型能存储的最大值是？
     // The maximum value an int type can store
